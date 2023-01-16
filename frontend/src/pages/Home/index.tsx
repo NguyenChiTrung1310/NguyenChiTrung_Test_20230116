4bde890b8da3958b65ba26f47d6224a45e81e577
Trung Nguyen <nguyenchitrung1310@gmail.com>
2023-01-16 22:42:57 +0700

    Create and custom pie chart

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import BannerLeft from '@/assets/images/home/d01.jpg';
 import BannerRight from '@/assets/images/home/main_graph.png';
+import Pie from '@/components/Pie';
 
 const HomePage = () => {
   return (
@@ -11,6 +12,9 @@ const HomePage = () => {
             src={BannerLeft}
             className='object-cover absolute inset-0 h-auto max-h-full w-full'
           />
+          <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+            <Pie percentage={75} colour='white' />
+          </div>
         </div>
         <div className='w-full tablet:w-[57.8125%] bg-dark-600 h-full flex-center'>
           <img alt='top-page-img-right' src={BannerRight} />
