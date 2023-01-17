3ae25c8d9d264caa97e2522077f96149b7c3c9d8
Trung Nguyen <nguyenchitrung1310@gmail.com>
2023-01-17 17:50:07 +0700

    Update login page

--- frontend/src/pages/LoginPage/index.tsx
+++ frontend/src/pages/LoginPage/index.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+
+import { useAuth } from '@/context/Auth';
+import { ROUTES } from '@/utils/routes';
+
+import Button from '@/components/@cores/Button';
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const { onLogin, token } = useAuth();
+  const navigate = useNavigate();
+
+  if (token) {
+    navigate('/');
+  }
+
+  return (
+    <div className='w-full'>
+      <div className='mt-14 block w-fit mx-auto'>
+        <Button onClick={onLogin} className=''>
+          Login
+        </Button>
+
+        <div className='mt-14'>
+          <NavLink to={ROUTES.COLUMNS} className='text-primary'>
+            Go to Columns Page
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
