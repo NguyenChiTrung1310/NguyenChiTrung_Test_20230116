1f5979064b5537912d09e87711b6cea20e91c563
Trung Nguyen <nguyenchitrung1310@gmail.com>
2023-01-17 17:53:42 +0700

    Implement logout function

--- frontend/src/components/header/MenuBox/index.tsx
+++ frontend/src/components/header/MenuBox/index.tsx
@@ -1,4 +1,6 @@
+import { useAuth } from '@/context/Auth';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface Props {
   position: {
@@ -8,6 +10,12 @@ interface Props {
 }
 
 const MenuBox: React.FC<Props> = ({ position }) => {
+  const navigate = useNavigate();
+  const { onLogout, token } = useAuth();
+
+  if (token) {
+    navigate('/login');
+  }
   return (
     <div
       style={{
@@ -16,7 +24,14 @@ const MenuBox: React.FC<Props> = ({ position }) => {
       }}
       className='absolute w-[280px] h-auto bg-black text-white'
     >
-      MenuBox
+      <ul>
+        <li>Menu</li>
+        {token && (
+          <li className='cursor-pointer'>
+            <p onClick={onLogout}>Logout</p>
+          </li>
+        )}
+      </ul>
     </div>
   );
 };
